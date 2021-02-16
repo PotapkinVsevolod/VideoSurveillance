d8b090f992609075018ffa85f4841ff97481a5c5
potapkinvsevolod <potapkinvsevolod@gmail.com>
2021-02-16 21:32:04 +0700

    Add tests. Main - to OOP.

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ CAMERA_URL = 'rtsp://admin:@192.168.1.10'
 CLOSING_KEY = 'q'
 
 
-def start_camera():
-    capture = cv2.VideoCapture(CAMERA_URL)
+class Camera:
+    def __init__(self, url):
+        self.url = url
+        self.capture = cv2.VideoCapture(self.url)
 
-    while True:
-        ret, frame = capture.read()
+    def get_frame(self):
+        ret, frame = self.capture.read()
+        return ret, frame
+
+    def show_frame(self, frame):
         cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord(CLOSING_KEY):
-            break
 
-    capture.release()
-    cv2.destroyAllWindows()
+    def stop_condition(self):
+        return cv2.waitKey(1) & 0xFF == ord(CLOSING_KEY)
+
+    def close_windows_and_clean_memory(self):
+        self.capture.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    start_camera()
+    pass
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,5 +1,18 @@
 import unittest
 
+from cv2 import VideoCapture
+
+from main import Camera, CAMERA_URL
+
 
 class TestCamera(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.camera = Camera(url=CAMERA_URL)
+
+    def test_capture(self):
+        self.assertEqual(type(self.camera.capture), VideoCapture)
+
+    def test_not_empty_frame(self):
+        ret, frame = self.camera.get_frame()
+        self.assertTrue(ret)
+        self.assertIsNotNone(frame)
