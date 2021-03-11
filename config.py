8cdcd0469482f75d59cb3486a1344713fa23572f
potapkinvsevolod <depas123e26>
2021-03-11 10:23:54 +0700

    Коммит, чтобы показать Илье.

---
+++ config.py
@@ -0,0 +1,13 @@
+HOME_CAMERA = {
+    'ip':'192.168.1.10',
+    'login':'admin',
+    'password': '',
+    'port': '554',
+}
+
+REMOTE_CAMERA = {
+    'ip': '178.176.243.64',
+    'login': 'admin',
+    'password': 'AdminF13',
+    'port': '8011',
+}
